83b5278eb44ed4edecd3b43aa2421aa7b6a94549
daiki-nakajima <stovepipe531@gmail.com>
2019-04-26 01:17:53 +0900

    fix devServer

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,9 @@ module.exports = {
         extensions: [ '.ts', '.js' ]
     },
     output: {
-        filename: 'app.js',                             // バンドルのファイル名。[name]はentryで指定したキー
-        path: path.resolve(__dirname, 'src'),           // ファイルの配置パス
-        publicPath: "/",                                // ブラウザからバンドルにアクセスする際のパス
+        filename: 'app.js',                            // バンドルのファイル名。[name]はentryで指定したキー
+        path: path.resolve(__dirname, 'src'),          // ファイルの配置パス
+        publicPath: "/",                               // ブラウザからバンドルにアクセスする際のパス
         // library: ["com", "example"], // パッケージ名を配列で表現する
         // libraryTarget: 'umd'
     },
@@ -28,10 +28,10 @@ module.exports = {
 
     //webpack-dev-server用設定
     devServer: {
-        // open: true,                                   // ブラウザを自動で開く
-        // openPage: "index.html",                       // 自動で指定したページを開く
-        contentBase: path.resolve(__dirname, 'src'),    // HTML等コンテンツのルートディレクトリ
-        // watchContentBase: true,                       // コンテンツの変更監視をする
+        open: true,                                   // ブラウザを自動で開く
+        openPage: "index.html"  ,                     // 自動で指定したページを開く
+        contentBase: path.resolve(__dirname, 'src'),  // HTML等コンテンツのルートディレクトリ
+        watchContentBase: true,                       // コンテンツの変更監視をする
         port: 3000,                                   // ポート番号
     }
 
