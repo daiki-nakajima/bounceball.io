659ec40c079410f2a1246fb4aefd4a59520cc38b
daiki-nakajima <stovepipe531@gmail.com>
2019-05-30 03:37:54 +0900

    wip

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 'use strict';
 
-const express = require('express');
-const http = require('http');
-// const socket = require('socket.io');
-// const Game = require('./libs/Game');
-
-const app = express();
-const server = http.Server(app);
-// const io = socket(server);
-// const game = new Game();
-
-// game.start(io);
-app.use(express.static(__dirname + '/public'));
-
+const WebSocketServer = require('ws').Server;
 const port = process.env.PORT || 3000;
+ 
+// setting ws
+const wss = new WebSocketServer({port:port});
+console.log("websocket server created");
 
-server.listen(port, () => {
-    console.log('server is listening on port ' + port)
+// 接続リスト
+let connections = [];
+ 
+// WebSocketServer起動
+wss.on('connection', (ws) => {
+    console.log('websocket connection open');
+    // ws受信時、接続リストを追加
+    connections.push(ws);
+    // 'message' という Key で受信したら、Dataを全クライアントに送信
+    ws.on('message', (data) => {
+        console.log('received: %s', message); 
+        wss.clients.forEach((client) => {
+        client.send("ユーザー" + (connections.indexOf(ws) + 1) +"からのメッセージです。" + data);
+        });
+    });
+    
+    // クローズ
+    ws.on('close', () => {
+        console.log('websocket connection close');
+    });
 });
