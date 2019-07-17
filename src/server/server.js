13d023d490391c380556ff1492721efa7b0e04ad
daiki-nakajima <stovepipe531@gmail.com>
2019-07-17 23:22:14 +0900

    fix page

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ const server = http.Server(app);
 const io = socketIO(server);
 
 // ポート番号（環境変数PORTがあればそれを、無ければ1337を使う）
-const PORT_NO = process.env.PORT || 1337;
+const PORT_NO = process.env.PORT || 80;
 
 // ゲームの作成と開始
 const game = new Game();
